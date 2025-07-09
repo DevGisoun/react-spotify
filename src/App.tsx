a23016b5a1a31747463f506e4fdf0dc602abba3a
DevGisoun <devgisoun@gmail.com>
2025-07-09 00:25:43 +0000

    feat: vite-plugin-svgr을 이용한 SVG 컴포넌트화

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import './App.css';
+import Logo from '@assets/logo.svg?react';
 
 function App() {
     return (
         <>
-            <div></div>
+            {/* Nav Bar */}
+            <div className="flex flex-row justify-between items-center p-2">
+                {/* 로고, 홈 버튼, 검색바 영역 */}
+                <div>
+                    <Logo width={50} height={50} fill="white" />
+                </div>
+                {/* 서비스 지원 영역 */}
+            </div>
         </>
     );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+// vite.config.js
+import svgr from 'vite-plugin-svgr';
+
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), svgr()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@comps': path.resolve(__dirname, './src/components'),
         },
     },
 });
